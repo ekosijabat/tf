4ec04ecba5937b6e67a88c2803078369d3e09d43
ekosijabat <my@personal>
2023-03-29 16:34:39 +0800

    second

--- modules/database/mysql/output.tf
+++ modules/database/mysql/output.tf
@@ -1,6 +1,6 @@
-output "rds_template" {
-  value = data.template_file.user_data.rendered
-}
+#output "rds_template" {
+#  value = data.template_file.user_data.rendered
+#}
 
 output "rds_endpoint" {
   value = aws_db_instance.wordpressdb.endpoint
